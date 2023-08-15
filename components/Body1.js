909d19abe9f094122a9526566662b310021c6f4b
ImadMustafa <137775274+ImadMustafa@users.noreply.github.com>
2023-08-15 13:30:44 +0300

    Updated website with new changes

--- components/Body1.js
+++ components/Body1.js
@@ -1,11 +1,11 @@
 const Body1 = ({title, text, imageUrl }) => {
     return (
-        <div class="text-container mx-4 my-8 grid 2xl:container mx-auto">
+        <div class="text-container my-8 flex 2xl:container mx-auto grid-cols-2">
         <div class="text w-5/6 md:w-90 p-4">
             <h2 class="font-bold text-2xl">{title}</h2>
             <p>{text}</p>
         </div>
-        <div class="picture w-1/2 md:w-50  justify-center">
+        <div class="picture w-1/2 md:w-50 justify-center">
             <img src={imageUrl} width="350px" alt="Image" />
         </div>
     </div>
