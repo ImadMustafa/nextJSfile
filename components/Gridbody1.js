0bb1cc3151660d4452a4771cf6bb1e0fa3ad5494
ImadMustafa <137775274+ImadMustafa@users.noreply.github.com>
2023-08-15 10:55:19 +0300

    Updated website with new changes

--- components/Gridbody1.js
+++ components/Gridbody1.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Gridbody1 = ({ items }) => {
   return (
-    <div className="flex flex-col items-center mt-10 mb-10 md:flex-row md:justify-center md:items-start 2xl:container mx-auto">
+    <div className="flex flex-col items-center mt-10 mb-10 md:flex-row md:justify-center md:items-start 2xl:container mx-auto grid-cols-3">
       {items.map((item, index) => (
         <div key={index} className="mb-8 md:mb-0 md:mx-10 ml-10 mr-10">
           <img src={item.imageUrl} className="flex justify-center h-60" alt="Image" />
