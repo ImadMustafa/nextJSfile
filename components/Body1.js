e303fa707ca258607bd2bd0016a82d062737a69f
ImadMustafa <137775274+ImadMustafa@users.noreply.github.com>
2023-08-17 15:07:25 +0300

    Updated website with new changes

--- components/Body1.js
+++ components/Body1.js
@@ -1,11 +1,11 @@
 const Body1 = ({title, text, imageUrl }) => {
     return (
-        <div class=" sm:text-2xl text-xl text-container my-8 2xl:container mx-auto grid grid-cols-2">
-        <div class="text w-5/6 md:w-90 p-4">
+        <div class=" sm:text-2xl text-xl text-container my-8 2xl:container mx-auto grid sm:grid-cols-2">
+        <div class="text w-5/6 md:w-90 p-4 mx-auto">
             <h2 class="font-bold text-2xl">{title}</h2>
             <p>{text}</p>
         </div>
-        <div class="picture w-1/2 md:w-50 justify-center">
+        <div class="picture w-1/2 md:w-50 justify-center mx-auto">
             <img src={imageUrl} width="350px" alt="Image" />
         </div>
     </div>
--- components/Form.js
+++ components/Form.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Form = ({formTitle}) => {
     return ( 
-        <div className="flex justify-center ml-6">
-            <div className="form my-8 bg-rose-100 rounded-lg p-6">
+        <div className="flex justify-center ml-6 mr-6 bg-rose-100 rounded-lg sm:bg-white">
+            <div className="form my-8 sm:bg-rose-100 rounded-lg p-6 w-96 ml-9">
                 <h2 className="padder mb-5 text-xl font-bold">{formTitle}</h2>
                 Email: <input type="text" placeholder="Your email" className="padder bg-white border-2 border-white rounded-lg p-2 mb-3" />
-                <p className="mb-3">Message:</p><textarea name="" className="padder bg-white border-2 border-white rounded-lg p-2 mb-3" cols="50" rows="7" placeholder="Write your message here."></textarea>
+                <p className="mb-3">Message:</p><textarea name="" className="padder bg-white border-2 border-white rounded-lg p-2 mb-3" cols="40" rows="7" placeholder="Write your message here."></textarea>
                 <div>
                 <button style={{ width: '80px' }} className="padder bg-white text-gray-700 rounded-lg h-8">Send Email</button>
                 </div>
